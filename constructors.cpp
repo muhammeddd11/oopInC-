a5feb9e934cf720d62af64ff53e2996f18cf1cc8
muhammeddd11 <muhammedfared11@gmail.com>
2024-07-25 15:40:06 +0200

    encapsulation

--- constructors.cpp
+++ constructors.cpp
@@ -1,34 +1,51 @@
-
 #include <iostream>
 #include <list>
 using namespace std;
 
-class youtubeChannel{
-public:	
-    //class properties
-	string name;
-	string ownerName;
-	int subscribeCount;
-	list<string> publishedVideos;
-    // constructor
-    youtubeChannel(string name,string ownerName){
+class youtubeChannel {
+private:
+    // Class properties
+    string name;
+    string ownerName;
+    int subscribeCount;
+    list<string> publishedVideos;
 
+public:
+    // Constructor
+    youtubeChannel(string name, string ownerName) {
         this->name = name;
         this->ownerName = ownerName;
         subscribeCount = 0;
+    }
+
+    // Class methods
+    void GetInfo() {
+        cout << "Name: " << name << endl;
+        cout << "Owner: " << ownerName << endl;
+        cout << "Subscribers: " << subscribeCount << endl;
+        cout << "Published Videos:" << endl;
+        for (string title : publishedVideos) {
+            cout << title << endl;
         }
-    // class methods
-    void GetInfo(){
-	    cout <<name<<endl;
-	    cout <<subscribeCount<<endl;
-	    for(string title: publishedVideos){
-	        cout<<title<<endl;
-	    }
+    }
+
+    void subscribe() {
+        subscribeCount++;
+    }
+
+    void unsubscribe() {
+        if (subscribeCount > 0)
+            subscribeCount--;
+    }
+
+    void publishVideo(string title) {
+        publishedVideos.push_back(title);
     }
 };
 
 int main() {
-	youtubeChannel ytChanne1("dailyCoding","muhammed abdelsamea");
-    ytChanne1.GetInfo();
-	return 0;
+    youtubeChannel ytChannel1("dailyCoding", "muhammed abdelsamea");
+    ytChannel1.GetInfo();
+    ytChannel1.subscribe();
+    return 0;
 }
